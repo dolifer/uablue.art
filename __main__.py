356c118bdd2a02fe953c75acb6b2aeda6ccaf5a6
Denis Olifer <denis@olifer.org>
2024-07-21 23:14:59 +0300

    Update zoneid and enable valid records

--- __main__.py
+++ __main__.py
@@ -1,28 +1,28 @@
 """An AWS Python Pulumi program"""
 import pulumi_cloudflare as cloudflare
 
-zone_id = "77fec8baef6d31bedd71e76f3b54954a" # Zone ID for olifer.org
+zone_id = "1033deaa825309b4ab6c39ebb4aa65b3" # Zone ID for uablue.art
 records = [
-    # {
-    #     "name": "_atproto.atrejane",
-    #     "value": "did=did:plc:cqub735qx3z2hqhmxyci6afb"
-    # },
-    # {
-    #     "name": "_atproto.breadhood",
-    #     "value": "did=did:plc:6gm3qvly7usvh5ffi3b3moae"
-    # },
-    #     {
-    #     "name": "_atproto.nukk3r",
-    #     "value": "did=did:plc:u6edaiy6nsfdtymyadrlpv6a"
-    # },
-    # {
-    #     "name": "_atproto.seamoreart",
-    #     "value": "did=did:plc:jefkyvwvsl57ptdjf2jpcldb"
-    # },
-    # {
-    #     "name": "_atproto.ypypy",
-    #     "value": "did=did:plc:hjmoaf4qr27t4afutdegggfh"
-    # }
+    {
+        "name": "_atproto.atrejane",
+        "value": "did=did:plc:cqub735qx3z2hqhmxyci6afb"
+    },
+    {
+        "name": "_atproto.breadhood",
+        "value": "did=did:plc:6gm3qvly7usvh5ffi3b3moae"
+    },
+        {
+        "name": "_atproto.nukk3r",
+        "value": "did=did:plc:u6edaiy6nsfdtymyadrlpv6a"
+    },
+    {
+        "name": "_atproto.seamoreart",
+        "value": "did=did:plc:jefkyvwvsl57ptdjf2jpcldb"
+    },
+    {
+        "name": "_atproto.ypypy",
+        "value": "did=did:plc:hjmoaf4qr27t4afutdegggfh"
+    }
 ]
 
 for record in records:
